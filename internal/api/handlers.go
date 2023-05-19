0cfc413ccedd434a48da120c72e2a5a1a1c0b533
Cristian Llorente <17942124+cristiancll@users.noreply.github.com>
2023-05-18 21:34:42 -0300

    Cleaning things up

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -8,9 +8,7 @@ type handlers struct {
 	wpp       handler.WhatsApp
 	category  handler.Category
 	item      handler.Item
-	opLog     handler.OperationLog
 	sale      handler.Sale
-	saleItem  handler.SaleItem
 	retrieval handler.Retrieval
 	sku       handler.SKU
 	stock     handler.Stock
@@ -22,9 +20,7 @@ func (s *Server) createHandlers() {
 	s.handlers.wpp = handler.NewWhatsApp(s.services.wpp)
 	s.handlers.category = handler.NewCategory(s.services.category)
 	s.handlers.item = handler.NewItem(s.services.item)
-	s.handlers.opLog = handler.NewOperationLog(s.services.opLog)
 	s.handlers.sale = handler.NewSale(s.services.sale)
-	s.handlers.saleItem = handler.NewSaleItem(s.services.saleItem)
 	s.handlers.retrieval = handler.NewRetrieval(s.services.retrieval)
 	s.handlers.sku = handler.NewSKU(s.services.sku)
 	s.handlers.stock = handler.NewStock(s.services.stock)
