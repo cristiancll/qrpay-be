74707528b15c8bd7a7d08c89299d475f9582322c
Cristian Llorente <17942124+cristiancll@users.noreply.github.com>
2023-07-13 19:41:12 -0300

    new error codes

--- internal/errCode/codes.go
+++ internal/errCode/codes.go
@@ -1,10 +1,39 @@
 package errCode
 
-import errs "github.com/cristiancll/go-errors"
+import (
+	errs "github.com/cristiancll/go-errors"
+	"google.golang.org/grpc/codes"
+)
 
 const (
-	INTERNAL errs.ErrorCode = iota + 1
-	NOT_FOUND
-	NOT_CHANGED
-	ACCESS_DENIED
+	Unknown errs.Code = iota
+	Internal
+	NotFound
+	NotChanged
+	AccessDenied
+	AlreadyExists
+	Unauthorized
+	Unauthenticated
+	InvalidArgument
 )
+
+func ToGRPCCode(code errs.Code) codes.Code {
+	switch code {
+	case Internal:
+		return codes.Internal
+	case NotFound:
+		return codes.NotFound
+	case NotChanged:
+		return codes.FailedPrecondition
+	case AccessDenied:
+		return codes.PermissionDenied
+	case AlreadyExists:
+		return codes.AlreadyExists
+	case Unauthorized:
+		return codes.Unauthenticated
+	case Unauthenticated:
+		return codes.Unauthenticated
+	default:
+		return codes.Unknown
+	}
+}
